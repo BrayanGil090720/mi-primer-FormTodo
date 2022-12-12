8f631f1ffe47952d63c60329d1f504ede3e1236b
RiderDev <lx04227@innovaccion.mx>
2022-12-12 17:28:28 -0600

    se agrego el checkbox

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-const Checkbox =() =>{
+const Checkbox =props =>{
+
+    const {
+        onChange,
+        data: {id, description, done}
+    } = props;
     return(
-        <div>
-            checkbox
-        </div>
+      <Fragment>
+        <label className="todo new-item"></label>
+        <input className="todo_state" name={id} type="checkbox" defaultChecked={done} onChange={onChange}/>
+        <div className="todo_text">{description}</div>
+      </Fragment>
     );
 }
 
